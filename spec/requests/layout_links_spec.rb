0e11637ef979b549c546de110f1bb32d0ef7fa04
Mitchell Bagley <bagleymj@gmail.com>
2012-02-28 15:42:36 -0500

    Fixed Rspec/Rake Compatibility Issue

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -27,4 +27,18 @@ describe "LayoutLinks" do
 		response.should have_selector('title', :content => "Sign up")
 	end
 
+	it "should have the right links on the layout" do
+		visit root_path
+		click_link "About"
+		response.should be_success
+		click_link "Help"
+		response.should be_success
+		click_link "Contact"
+		response.should be_success
+		click_link "Home"
+		response.should be_success
+		click_link "Sign up now!"
+		response.should be_success
+	end
+
 end
